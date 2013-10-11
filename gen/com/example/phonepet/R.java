4864d06ffb7a6047202db8b32f14ee0d60e8dc71
zellarsm <mzellars822@yahoo.com>
2013-10-11 09:36:32 -0400

    Began learning about animation this morning
    -Created animation folder in \res. This is where all animation xml files
    will go.
    -Created two animation xml files and placed them in anim folder. These
    files will animate a popup window
    -All animations must be added to styles.xml inside \values so they can
    be referenced throughout the project
    -Created layout of the actual popup
    
    Reference:
    http://thedeveloperworldisyours.com/android/pop-up-in-android/

--- gen/com/example/phonepet/R.java
+++ gen/com/example/phonepet/R.java
@@ -8,7 +8,16 @@
 package com.example.phonepet;
 
 public final class R {
+    public static final class anim {
+        public static final int popup_hide=0x7f040000;
+        public static final int popup_show=0x7f040001;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int color_popup=0x7f070000;
+        public static final int color_popup_outer_window=0x7f070002;
+        public static final int color_popup_window=0x7f070001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,43 +26,58 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int mikesoccerball=0x7f020001;
-        public static final int pooper_scooper=0x7f020002;
-        public static final int smileyface=0x7f020003;
-        public static final int sponge=0x7f020004;
-        public static final int taco=0x7f020005;
-        public static final int templatebackground=0x7f020006;
-        public static final int tennisball=0x7f020007;
-        public static final int tshirt=0x7f020008;
+        public static final int dummy_egg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int mikesoccerball=0x7f020002;
+        public static final int pooper_scooper=0x7f020003;
+        public static final int smileyface=0x7f020004;
+        public static final int sponge=0x7f020005;
+        public static final int taco=0x7f020006;
+        public static final int templatebackground=0x7f020007;
+        public static final int tennisball=0x7f020008;
+        public static final int tshirt=0x7f020009;
     }
     public static final class id {
-        public static final int HomeView=0x7f080001;
-        public static final int action_settings=0x7f080002;
-        public static final int activity_home_view=0x7f080000;
+        public static final int HomeView=0x7f0a0003;
+        public static final int action_settings=0x7f0a0007;
+        public static final int activity_home_view=0x7f0a0002;
+        public static final int close=0x7f0a0005;
+        public static final int img_green_egg=0x7f0a0001;
+        public static final int popupLinearLayout=0x7f0a0004;
+        public static final int txt_pick_egg=0x7f0a0000;
+        public static final int txt_welcome=0x7f0a0006;
     }
     public static final class layout {
         public static final int activity_create=0x7f030000;
         public static final int activity_home=0x7f030001;
         public static final int activity_startup=0x7f030002;
+        public static final int popup_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int create=0x7f070000;
-        public static final int home=0x7f070001;
-        public static final int startup=0x7f070002;
+        public static final int create=0x7f090000;
+        public static final int home=0x7f090001;
+        public static final int startup=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int pet_name=0x7f050004;
-        public static final int preference_file_key=0x7f050005;
-        public static final int title_activity_create=0x7f050006;
-        public static final int title_activity_home=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        /**  Strings related to Popup Window Animation 
+         */
+        public static final int close_button=0x7f060009;
+        public static final int hello_world=0x7f060002;
+        public static final int img_green_egg=0x7f060008;
+        public static final int pet_name=0x7f060004;
+        /**  Strings related to CreateActivity 
+         */
+        public static final int pick_egg=0x7f060007;
+        public static final int preference_file_key=0x7f060005;
+        public static final int title_activity_create=0x7f060006;
+        public static final int title_activity_home=0x7f060003;
+        public static final int welcome=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -77,10 +101,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        /**  Animations 
+         */
+        public static final int PopupWindowAnimation=0x7f080002;
     }
 }
