efefa2c1b29f670bbfd03659aeffe2587c37d808
happygilmore88 <happygilmore88@gmail.com>
2013-11-08 18:54:58 -0500

    Test commit and adding clean activity

---
+++ src/com/example/phonepet/CleanActivity.java
@@ -0,0 +1,19 @@
+package com.example.phonepet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class CleanActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    Log.v("Clean Activity", "This works!");
+	    
+	    // TODO Auto-generated method stub
+	}
+
+}
