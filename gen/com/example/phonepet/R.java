c6cb586214de5db949403a36f3cc2d93584d37fe
dennisfs <dennis_fs@hotmail.com>
2013-11-09 20:29:10 -0500

    Revert "Correctly added CleanActivity"
    This reverts commit 107ff89af63f97afde7812c5789d779677c436a7.

--- gen/com/example/phonepet/R.java
+++ gen/com/example/phonepet/R.java
@@ -13,20 +13,11 @@ public final class R {
         public static final int popup_show=0x7f040001;
     }
     public static final class attr {
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarButtonStyle=0x7f010001;
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f050000;
-        public static final int color_popup=0x7f050001;
-        public static final int color_popup_outer_window=0x7f050003;
-        public static final int color_popup_window=0x7f050002;
+        public static final int color_popup=0x7f070000;
+        public static final int color_popup_outer_window=0x7f070002;
+        public static final int color_popup_window=0x7f070001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -35,8 +26,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int acc_black_bowtie=0x7f020000;
@@ -79,61 +70,57 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int tshirt2=0x7f020025;
     }
     public static final class id {
-        public static final int Accessorize=0x7f0a0021;
-        public static final int EnergyText=0x7f0a001f;
-        public static final int Feed=0x7f0a0022;
-        public static final int HappinessText=0x7f0a001d;
-        public static final int HomeView=0x7f0a0020;
-        public static final int HungerText=0x7f0a001e;
-        public static final int Play=0x7f0a0023;
-        public static final int Poop=0x7f0a0024;
-        public static final int RunawayOKButton=0x7f0a0027;
-        public static final int Sponge=0x7f0a0025;
-        public static final int StatusBar=0x7f0a001c;
-        public static final int TestButton=0x7f0a0026;
-        public static final int action_settings=0x7f0a002b;
+        public static final int Accessorize=0x7f0a001e;
+        public static final int EnergyText=0x7f0a001c;
+        public static final int Feed=0x7f0a001f;
+        public static final int HappinessText=0x7f0a001a;
+        public static final int HomeView=0x7f0a001d;
+        public static final int HungerText=0x7f0a001b;
+        public static final int Play=0x7f0a0020;
+        public static final int Poop=0x7f0a0021;
+        public static final int RunawayOKButton=0x7f0a0024;
+        public static final int Sponge=0x7f0a0022;
+        public static final int StatusBar=0x7f0a0019;
+        public static final int TestButton=0x7f0a0023;
+        public static final int action_settings=0x7f0a0028;
         public static final int activity_accessorize_view=0x7f0a0000;
-        public static final int activity_home_view=0x7f0a001b;
-        public static final int background_grid_layout=0x7f0a0012;
+        public static final int activity_home_view=0x7f0a0018;
+        public static final int background_grid_layout=0x7f0a000f;
         public static final int blackTie=0x7f0a000b;
         public static final int blueTie=0x7f0a000d;
         public static final int bowtie=0x7f0a000e;
         public static final int clearButton=0x7f0a0004;
-        public static final int close=0x7f0a0029;
-        public static final int connect4View=0x7f0a0018;
-        public static final int depth_add_btn=0x7f0a0014;
-        public static final int depth_minus_btn=0x7f0a0015;
-        public static final int dummy_button=0x7f0a0011;
-        public static final int fullscreen_content=0x7f0a000f;
-        public static final int fullscreen_content_controls=0x7f0a0010;
+        public static final int close=0x7f0a0026;
+        public static final int connect4View=0x7f0a0015;
+        public static final int depth_add_btn=0x7f0a0011;
+        public static final int depth_minus_btn=0x7f0a0012;
         public static final int glasses=0x7f0a000a;
         public static final int horizontalScrollView=0x7f0a0007;
-        public static final int humanFirstCB=0x7f0a0017;
-        public static final int img_green_egg=0x7f0a001a;
+        public static final int humanFirstCB=0x7f0a0014;
+        public static final int img_green_egg=0x7f0a0017;
         public static final int inner_scroll_layout=0x7f0a0006;
         public static final int linearLayout1=0x7f0a0002;
         public static final int pet_image=0x7f0a0005;
         public static final int pet_image_layout=0x7f0a0001;
-        public static final int plidDepth=0x7f0a0016;
-        public static final int popupLinearLayout=0x7f0a0028;
+        public static final int plidDepth=0x7f0a0013;
+        public static final int popupLinearLayout=0x7f0a0025;
         public static final int redTie=0x7f0a000c;
         public static final int saveButton=0x7f0a0003;
-        public static final int start_btn=0x7f0a0013;
+        public static final int start_btn=0x7f0a0010;
         public static final int topHat=0x7f0a0008;
-        public static final int txt_pick_egg=0x7f0a0019;
-        public static final int txt_welcome=0x7f0a002a;
+        public static final int txt_pick_egg=0x7f0a0016;
+        public static final int txt_welcome=0x7f0a0027;
         public static final int unicornHorn=0x7f0a0009;
     }
     public static final class layout {
         public static final int activity_accessorize=0x7f030000;
-        public static final int activity_clean=0x7f030001;
-        public static final int activity_connect4=0x7f030002;
-        public static final int activity_create=0x7f030003;
-        public static final int activity_home=0x7f030004;
-        public static final int activity_play=0x7f030005;
-        public static final int activity_runaway=0x7f030006;
-        public static final int activity_startup=0x7f030007;
-        public static final int popup_layout=0x7f030008;
+        public static final int activity_connect4=0x7f030001;
+        public static final int activity_create=0x7f030002;
+        public static final int activity_home=0x7f030003;
+        public static final int activity_play=0x7f030004;
+        public static final int activity_runaway=0x7f030005;
+        public static final int activity_startup=0x7f030006;
+        public static final int popup_layout=0x7f030007;
     }
     public static final class menu {
         public static final int accessorize=0x7f090000;
@@ -145,40 +132,37 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int startup=0x7f090006;
     }
     public static final class string {
-        public static final int access_desc=0x7f070015;
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int clear_button=0x7f070010;
+        public static final int access_desc=0x7f060015;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int clear_button=0x7f060010;
         /**  Strings related to Popup Window Animation 
          */
-        public static final int close_button=0x7f070011;
-        public static final int dummy_button=0x7f070018;
-        public static final int dummy_content=0x7f070019;
-        public static final int food_button=0x7f070008;
-        public static final int hello_world=0x7f070002;
-        public static final int img_green_egg=0x7f07000e;
-        public static final int pet_name=0x7f070004;
+        public static final int close_button=0x7f060011;
+        public static final int food_button=0x7f060008;
+        public static final int hello_world=0x7f060002;
+        public static final int img_green_egg=0x7f06000e;
+        public static final int pet_name=0x7f060004;
         /**  Strings related to CreateActivity 
          */
-        public static final int pick_egg=0x7f07000d;
-        public static final int play_button=0x7f070009;
-        public static final int poop_button=0x7f07000a;
-        public static final int preference_file_key=0x7f070005;
+        public static final int pick_egg=0x7f06000d;
+        public static final int play_button=0x7f060009;
+        public static final int poop_button=0x7f06000a;
+        public static final int preference_file_key=0x7f060005;
         /**  Strings related to AccessorizeActivity 
          */
-        public static final int save_button=0x7f07000f;
+        public static final int save_button=0x7f06000f;
         /**  Strings related to HomeActivity Menu buttons 
          */
-        public static final int shirt_button=0x7f070007;
-        public static final int sponge_button=0x7f07000b;
-        public static final int test_button=0x7f07000c;
-        public static final int title_activity_accessorize=0x7f070014;
-        public static final int title_activity_clean=0x7f070017;
-        public static final int title_activity_create=0x7f070006;
-        public static final int title_activity_home=0x7f070003;
-        public static final int title_activity_play=0x7f070013;
-        public static final int title_activity_runaway=0x7f070016;
-        public static final int welcome=0x7f070012;
+        public static final int shirt_button=0x7f060007;
+        public static final int sponge_button=0x7f06000b;
+        public static final int test_button=0x7f06000c;
+        public static final int title_activity_accessorize=0x7f060014;
+        public static final int title_activity_create=0x7f060006;
+        public static final int title_activity_home=0x7f060003;
+        public static final int title_activity_play=0x7f060013;
+        public static final int title_activity_runaway=0x7f060016;
+        public static final int welcome=0x7f060012;
     }
     public static final class style {
         /** 
@@ -207,54 +191,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
-        public static final int ButtonBar=0x7f080004;
-        public static final int ButtonBarButton=0x7f080005;
-        public static final int FullscreenActionBarStyle=0x7f080006;
-        public static final int FullscreenTheme=0x7f080003;
         /**  Animations 
          */
         public static final int PopupWindowAnimation=0x7f080002;
     }
-    public static final class styleable {
-        /** 
-         Declare custom theme attributes that allow changing which styles are
-         used for button bars depending on the API level.
-         ?android:attr/buttonBarStyle is new as of API 11 so this is
-         necessary to support previous API levels.
-    
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.example.phonepet:buttonBarButtonStyle}</code></td><td></td></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.example.phonepet:buttonBarStyle}</code></td><td></td></tr>
-           </table>
-           @see #ButtonBarContainerTheme_buttonBarButtonStyle
-           @see #ButtonBarContainerTheme_buttonBarStyle
-         */
-        public static final int[] ButtonBarContainerTheme = {
-            0x7f010000, 0x7f010001
-        };
-        /**
-          <p>This symbol is the offset where the {@link com.example.phonepet.R.attr#buttonBarButtonStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name com.example.phonepet:buttonBarButtonStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
-        /**
-          <p>This symbol is the offset where the {@link com.example.phonepet.R.attr#buttonBarStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name com.example.phonepet:buttonBarStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
-    };
 }
